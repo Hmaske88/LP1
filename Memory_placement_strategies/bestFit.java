3f1c8f749b866c65fef1f5eadc0bde2a2a64c821
Hmaske88 <89325376+Hmaske88@users.noreply.github.com>
2022-11-18 20:09:46 +0530

    Create bestFit.java

---
+++ Memory_placement_strategies/bestFit.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class bestFit
+{
+    public static void main(String arg[])
+    {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of partitions : ");
+        int npar=sc.nextInt();
+        
+        int part[]=new int[npar];
+        System.out.println("Enter the size of partitions : ");
+        for(int i=0;i<npar;i++)
+        {
+            part[i]=sc.nextInt();    
+        }
+        
+        System.out.println("Enter the number of processes : ");
+        int npro=sc.nextInt();
+        
+        int pro[]=new int[npro];
+        System.out.println("Enter the size of processes : ");
+        for(int i=0;i<npro;i++)
+        {
+            pro[i]=sc.nextInt();
+        }
+        
+        int temp;
+        for(int i=0;i<npar-1;i++)
+        {
+            for(int j=i+1;j<npar;j++)
+            {
+                if(part[i]>part[j])
+                {
+                    temp=part[i];
+                    part[i]=part[j];
+                    part[j]=temp;
+                }
+            }
+        }
+        
+        for(int i=0;i<npro;i++)
+        {
+            for(int j=0;j<npar;j++)
+            {
+                if(pro[i]<=part[j])
+                {
+                    System.out.println("Process of size"+ pro[i]+" is placed in partition "+part[j]);
+                    part[j]=part[j]-pro[i];
+                    break;
+                }
+            }
+        }
+        
+    }
+}
