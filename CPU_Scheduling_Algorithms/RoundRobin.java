082fdec802ac3d3e98a006297c2ce4934d951695
Hmaske88 <89325376+Hmaske88@users.noreply.github.com>
2022-11-19 12:55:46 +0530

    Update RoundRobin.java

--- CPU_Scheduling_Algorithms/RoundRobin.java
+++ CPU_Scheduling_Algorithms/RoundRobin.java
@@ -1,62 +1,69 @@
-import java.util.Scanner;  
-    public class RoundRobin  
-    {  
-    public static void main(String args[])  
-    {  
-    int n,i,qt,count=0,temp,sq=0,bt[],wt[],tat[],rem_bt[];  
-    float awt=0,atat=0;  
-    bt = new int[10];  
-    wt = new int[10];  
-    tat = new int[10];  
-    rem_bt = new int[10];  
-    Scanner s=new Scanner(System.in);  
-    System.out.print("Enter the number of process (upto 10) : ");  
-    n = s.nextInt();  
-    System.out.print("Enter the burst time of the process\n");  
-    for (i=0;i<n;i++)  
-    {  
-    System.out.print("P"+i+" = ");   
-    bt[i] = s.nextInt();  
-    rem_bt[i] = bt[i];  
-    }  
-    System.out.print("Enter the quantum time: ");  
-    qt = s.nextInt();  
-    while(true)  
-    {  
-    for (i=0,count=0;i<n;i++)  
-    {  
-    temp = qt;  
-    if(rem_bt[i] == 0)  
-    {  
-    count++;  
-    continue;  
-    }  
-    if(rem_bt[i]>qt)  
-    rem_bt[i]= rem_bt[i] - qt;  
-    else  
-    if(rem_bt[i]>=0)  
-    {  
-    temp = rem_bt[i];  
-    rem_bt[i] = 0;  
-    }  
-    sq = sq + temp;  
-    tat[i] = sq;  
-    }  
-    if(n == count)  
-    break;  
-    }  
-    
-    System.out.print("\nProcess\t     Burst Time\t    Turnaround Time\t Waiting Time\n");  
-    for(i=0;i<n;i++)  
-    {  
-    wt[i]=tat[i]-bt[i];  
-    awt=awt+wt[i];  
-    atat=atat+tat[i];  
-    System.out.print((i+1)+"\t\t "+bt[i]+"\t\t "+tat[i]+"\t\t\t "+wt[i]+"\n");  
-    }  
-    awt=(float)awt/n;  
-    atat=(float)atat/n;  
-    System.out.println("\nAverage waiting Time = "+awt+"\n");  
-    System.out.println("Average turnaround time = "+atat);  
-    }  
+import java.util.*;
+
+public class RoundRobin
+{
+    public static void main(String arg[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of processess :");
+        int n = sc.nextInt();
+        
+        int at[]=new int[n];
+        int bt[]=new int[n];
+        int pid[]=new int[n];
+        int wt[]=new int[n];
+        int tt[]=new int[n];
+        int f[]=new int[n];
+        int nbt[]=new int[n];
+        int avgwt=0,avgtt=0;
+        
+        for(int i =0;i<n;i++)
+        {
+            System.out.println("Enter the arrival time for process "+(i+1)+" :");
+            at[i]=sc.nextInt();
+            System.out.println("Enter the burst time for process "+(i+1)+" :");
+            bt[i]=sc.nextInt();     //bt will change after subtracting quantum time 
+            nbt[i]=bt[i];           //nbt stores value of initial burst time
+            pid[i]=i+1;
+            f[i]=0;
+        }
+        System.out.println("Enter the quantum time :");
+        int qt=sc.nextInt();
+        int processCount=0;
+        int st=0;
+        while(true)
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(bt[i]>qt && f[i]==0 && at[i]<=st)
+                {
+                    bt[i]=bt[i]-qt;
+                    st+=qt;
+                }
+                else if(bt[i]<=qt && f[i]==0 && at[i]<=st)
+                {
+                    st+=bt[i];
+                    bt[i]=0;
+                    f[i]=1;
+                    tt[i]=st-at[i];
+                    wt[i]=tt[i]-nbt[i];
+                    processCount++;
+                    avgwt+=wt[i];
+                    avgtt+=tt[i];
+                }
+            }
+            if(processCount==n)
+            {
+                break;
+            }
+        }
+        
+        System.out.println("pId  \t  at  \t  bt  \t  wt  \t  tt");
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(pid[i]+"  \t  "+at[i]+"  \t  "+nbt[i]+"  \t  "+wt[i]+"  \t  "+tt[i]);
+        }
+        System.out.println("Average waiting time :"+(float)avgwt/n);
+        System.out.println("Average turnaround time :"+(float)avgtt/n);
     }
+}
